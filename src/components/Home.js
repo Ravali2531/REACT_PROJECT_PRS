ac18f7ed7ef2be8045031165e6216a086ab0f38f
unknown <sandra7560@gmail.com>
2024-07-11 12:28:38 -0400

    modified header, footer and added logout option

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
-import MyFooter from './Footer';
+import MyFooter from './Footer.js';
 import { Footer } from 'flowbite-react';
 const Home = () => {
   return (
     <div>
       <Header />
-        <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/upload-book">Upload Book</Link></li>            
-            <li><Link to="/manage-books">Manage Books</Link></li>
-        </ul>
-      <MyFooter />
+        <MyFooter />
     </div>
   )
 }
