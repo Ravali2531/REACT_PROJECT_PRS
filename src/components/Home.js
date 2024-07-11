1ac26af9977c5ac6f7b07ee6bf805ec97dc3bcbb
Ravali2531 <ravalicanada3166@gmail.com>
2024-07-11 10:59:31 -0400

    Included search functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import MyFooter from './Footer';
 import { Footer } from 'flowbite-react';
+import BannerCard from './BannerCard';
+import FavBook from './FavBook';
+import SearchBar from './SearchBar';
 const Home = () => {
   return (
     <div>
       <Header />
-        <ul>
+        {/* <ul>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/upload-book">Upload Book</Link></li>            
             <li><Link to="/manage-books">Manage Books</Link></li>
-        </ul>
+        </ul> */}
         <div className='banner-container'>
           <div className='banner-content'>
             {/* Left side */}
@@ -22,10 +25,7 @@ const Home = () => {
               <p className='banner-description'>
                 Libraries are essential in a process of giving citizens access to knowledge. In digital times they are needed more than ever before. In times of the internet, everyone can visit a library without leaving home. It’s just a matter of opening a library website, and you can not only borrow an ebook but also ask the librarian an online question.
               </p>
-              <div className='banner-search'>
-                <input type="search" name='search' id='search' placeholder='Search a Book' className='search-input' />
-                <button className='search-button'>Search</button>
-              </div>
+              <SearchBar />
             </div>
 
             {/* Right side */}
