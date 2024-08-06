64a2e28bf08d5ca60cd0dd678c8122274b76d3ef
KandruPoojitha <poojithakandru.ed@gmail.com>
2024-08-06 15:14:06 -0400

    fixed footer contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,44 +8,14 @@ const MyFooter = () => {
     <Footer bgDark>
       <div className="footer-container">
         <div className="footer-content">
-          <div>
-            <Footer.Title title="Company" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">About</Footer.Link>
-              <Footer.Link href="#">Careers</Footer.Link>
-              <Footer.Link href="#">Brand Center</Footer.Link>
-              <Footer.Link href="#">Blog</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="Help Center" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">Discord Server</Footer.Link>
-              <Footer.Link href="#">Twitter</Footer.Link>
-              <Footer.Link href="#">Facebook</Footer.Link>
-              <Footer.Link href="#">Contact Us</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="Legal" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">Privacy Policy</Footer.Link>
-              <Footer.Link href="#">Licensing</Footer.Link>
-              <Footer.Link href="#">Terms & Conditions</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="Download" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">iOS</Footer.Link>
-              <Footer.Link href="#">Android</Footer.Link>
-              <Footer.Link href="#">Windows</Footer.Link>
-              <Footer.Link href="#">MacOS</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
+          <Footer.Link href="#">About</Footer.Link>
+          <Footer.Link href="#">Privacy Policy</Footer.Link>
+          <Footer.Link href="#">Licensing</Footer.Link>
+          <Footer.Link href="#">Terms & Conditions</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
         </div>
         <div className="footer-bottom">
-        {<Footer.Copyright href="#" by="Flowbite™" year={2022} /> }
+          <Footer.Copyright href="#" by="BookStore" year={2024} />
           <div className="social-icons">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
