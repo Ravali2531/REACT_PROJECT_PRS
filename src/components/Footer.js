6249af5484444609cb9628e1e4846f8de37eb332
KandruPoojitha <poojithakandru.ed@gmail.com>
2024-08-07 12:15:28 -0400

    added contactUs to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,47 @@
-import React from 'react';
+// MyFooter.js
+import React, { useState } from 'react';
 import { Footer } from 'flowbite-react';
 import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
-import '../css/Footer.css'; 
+import '../css/Footer.css';
+import Chatbot from './Chatbot'; 
 
 const MyFooter = () => {
+  const [isChatbotVisible, setChatbotVisible] = useState(false);
+
+  const handleContactClick = (e) => {
+    e.preventDefault(); 
+    setChatbotVisible(true);
+  };
+
+  const handleCloseChatbot = () => {
+    setChatbotVisible(false);
+  };
+
   return (
-    <Footer bgDark>
-      <div className="footer-container">
-        <div className="footer-content">
-          <Footer.Link href="#">About</Footer.Link>
-          <Footer.Link href="#">Privacy Policy</Footer.Link>
-          <Footer.Link href="#">Licensing</Footer.Link>
-          <Footer.Link href="#">Terms & Conditions</Footer.Link>
-          <Footer.Link href="#">Contact Us</Footer.Link>
-        </div>
-        <div className="footer-bottom">
-          <Footer.Copyright href="#" by="BookStore" year={2024} />
-          <div className="social-icons">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+    <footer>
+      <Footer bgDark>
+        <div className="footer-container">
+          <div className="footer-content">
+            <Footer.Link href="#">About</Footer.Link>
+            <Footer.Link href="#">Privacy Policy</Footer.Link>
+            <Footer.Link href="#">Licensing</Footer.Link>
+            <Footer.Link href="#">Terms & Conditions</Footer.Link>
+            <Footer.Link href="#" onClick={handleContactClick}>Contact Us</Footer.Link>
+          </div>
+          <div className="footer-bottom">
+            <Footer.Copyright href="#" by="BookStore" year={2024} />
+            <div className="social-icons">
+              <Footer.Icon href="#" icon={BsFacebook} />
+              <Footer.Icon href="#" icon={BsInstagram} />
+              <Footer.Icon href="#" icon={BsTwitter} />
+              <Footer.Icon href="#" icon={BsGithub} />
+              <Footer.Icon href="#" icon={BsDribbble} />
+            </div>
           </div>
         </div>
-      </div>
-    </Footer>
+      </Footer>
+      <Chatbot isVisible={isChatbotVisible} onClose={handleCloseChatbot} />
+    </footer>
   );
 };
 
